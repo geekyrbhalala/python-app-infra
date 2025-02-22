add891f7d6ec5e529e4f43bb3bc714618d9a64cb
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-02-21 22:59:58 -0500

    update

--- infra/modules/security-groups/main.tf
+++ infra/modules/security-groups/main.tf
@@ -7,10 +7,6 @@ output "sg_ec2_sg_ssh_http_id" {
   value = aws_security_group.ec2_sg_ssh_http.id
 }
 
-output "rds_mysql_sg_id" {
-  value = aws_security_group.rds_mysql_sg.id
-}
-
 output "sg_ec2_for_python_api" {
   value = aws_security_group.ec2_sg_python_api.id
 
